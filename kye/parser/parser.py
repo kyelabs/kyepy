bf4cefb42bb93a3a68147b3e0ddfd6a77c9ab4b1
Benjamin Earl <14208829+benjameep@users.noreply.github.com>
2023-11-10 17:04:39 -0700

    put an api together for testing

--- kye/parser/parser.py
+++ kye/parser/parser.py
@@ -1,12 +1,19 @@
 from lark import Lark
 from pathlib import Path
 from kye.parser.kye_transformer import TreeToKye
-from lark import Transformer, Visitor, Tree, visitors
+from kye.parser.assign_scopes import assign_scopes, Scope
+from kye.parser.assign_type_refs import assign_type_refs
+from kye.parser.flatten_ast import flatten_ast
+from kye.dataset import Models
 
 DIR = Path(__file__).parent
 
 with open(DIR / 'grammar.lark') as f:
     grammar = f.read()
+
+GLOBAL_SCOPE = Scope(name=None, parent=None)
+for global_type in Models.globals.keys():
+    GLOBAL_SCOPE[global_type] = '<built-in type>'
 
 parser = Lark(
     grammar,
@@ -17,22 +24,26 @@ parser = Lark(
 )
 
 transformer = TreeToKye()
-class Parser:
-    def __init__(self, text):
-        self.text = text
-        self.tree = parser.parse(text)
-        self.ast = transformer.transform(self.tree)
-        print('hi')
 
-    @staticmethod
-    def from_text(text):
-        return Parser(text)
-    
-    @staticmethod
-    def from_file(file_path):
-        with open(file_path) as f:
-            text = f.read()
-        return Parser(text)
+def print_ast(ast):
+    FORMAT = '{:<20} {:<20} {}'
+    print(FORMAT.format('Scope', 'Type', 'Node'))
+    print('-'*80)
+    for path, node in ast.traverse():
+        print(FORMAT.format(
+            getattr(node.scope, 'path', '') or '',
+            node.type_ref or '',
+            '    '*(len(path)-1) + repr(node))
+        )
 
-    def print_tree(self):
-        print(self.tree.pretty())
+def kye_to_ast(text):
+    tree = parser.parse(text)
+    ast = transformer.transform(tree)
+    assign_scopes(ast, scope=GLOBAL_SCOPE)
+    assign_type_refs(ast)
+    return ast
+
+def compile(text):
+    ast = kye_to_ast(text)
+    raw_models = flatten_ast(ast)
+    return raw_models
--- main.py
+++ main.py
@@ -1,54 +1,14 @@
 from pathlib import Path
-from kye.parser.parser import Parser
-from kye.parser.assign_scopes import assign_scopes, Scope
-from kye.parser.assign_type_refs import assign_type_refs
-from kye.parser.flatten_ast import flatten_ast
-from pprint import pprint
-from kye.compiled import CompiledDataset
-from kye.dataset import Models
-from kye.loader.loader import Loader
-from kye.validate import Validate
-import duckdb
+import kye
 DIR = Path(__file__).parent
 
-def compile(file_path):
-    p = Parser.from_file(file_path)
-
-    GLOBAL_SCOPE = Scope(name=None, parent=None)
-    for global_type in ['Number','String','Boolean','Struct','Model']:
-        GLOBAL_SCOPE[global_type] = '<built-in type>'
-
-    assign_scopes(p.ast, scope=GLOBAL_SCOPE)
-    assign_type_refs(p.ast)
-
-    FORMAT = '{:<20} {:<20} {}'
-    print(FORMAT.format('Scope', 'Type', 'Node'))
-    print('-'*80)
-    for path, node in p.ast.traverse():
-        print(FORMAT.format(
-            getattr(node.scope, 'path', '') or '',
-            node.type_ref or '',
-            '    '*(len(path)-1) + repr(node))
-        )
-
-    raw_models = flatten_ast(p.ast)
-    pprint(raw_models)
-    return raw_models
-
-
 if __name__ == '__main__':
-    import sys
-    file_path = DIR / 'examples/yellow.kye'
-    if len(sys.argv) > 1:
-        file_path = sys.argv[1]
+    with open(DIR / 'examples/yellow.kye') as f:
+        text = f.read()
     
-    raw_models = compile(file_path)
-    models = CompiledDataset(models=raw_models)
-    models = Models(models)
-
-    loader = Loader(models)
-
-    loader.from_json('Yellow', [{
+    models = kye.compile(text)
+    
+    models.Yellow.from_records([{
         'id': 1,
         # 'size': 1,
         'meep': {
@@ -110,7 +70,7 @@ if __name__ == '__main__':
     }
     ])
 
-    loader.from_json('Yellow', [{
+    models.Yellow.from_records([{
         'id': 1.0,
         'size': None,
         'tags': ['hi', 'meep']
@@ -119,13 +79,11 @@ if __name__ == '__main__':
         'size': 3,
         'meep': { 'id': 3 },
     }])
-    
-    validate = Validate(loader)
 
-    for model_name, table in validate.tables.items():
+    for model_name, table in models.tables.items():
         print(model_name)
         print(table)
 
     print('Errors:')
-    print(validate.errors)
+    print(models.errors)
     print('hi')
