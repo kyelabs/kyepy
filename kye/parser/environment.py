aab8b833c20bd2de305773da2a7a2fa62eedd5f7
Benjamin Earl <14208829+benjameep@users.noreply.github.com>
2023-11-22 13:32:00 -0700

    allow environment path dot notation

--- kye/parser/environment.py
+++ kye/parser/environment.py
@@ -1,4 +1,13 @@
 from __future__ import annotations
+from typing import Optional
+
+# TODO: All of this fancy path parsing could probably lead to security vulnerabilities
+# if edge names are not properly sanitized. Should probably remove it from the released
+# version, and just keep it for debugging for now.
+def normalize_path(key: str) -> tuple[str]:
+    assert key != '', f'Environment path shouldn\'t be empty'
+    assert '..' not in key, f'Environment path shouldn\'t have a double period "{key}"'
+    return key.split('.')
 
 class Environment:
     local: dict[str, ChildEnvironment]
@@ -13,25 +22,39 @@ class Environment:
     @property
     def global_name(self) -> str:
         return '.'.join(self.path)
-    
-    def get_path(self, key) -> tuple[str]:
-        return (*self.resolve(key).path, key)
-    
-    def resolve(self, key) -> Environment:
-        if key in self.local:
+
+    def _resolve(self, key: str) -> Optional[Environment]:
+        """ Travel up the Environment hierarchy,
+        returning the environment that contains this key """
+        assert '.' not in key
+        if key in self.local or key == '':
             return self
-        raise KeyError(f'Could not resolve "{key}"')
+        return None
+    
+    def _descend(self, path: tuple[str]) -> Optional[Environment]:
+        """ Travel down the Environment hierarchy,
+        returning the environment at the end of this path """
+        if len(path) == 0:
+            return self
+        if path[0] == '':
+            return self._descend(path[1:])
+        if path[0] in self.local:
+            return self.local[path[0]]._descend(path[1:])
+        return None
 
-    def get(self, key, __default = None) -> ChildEnvironment:
-        return self.local.get(key, __default)
+    def _resolve_path(self, path: tuple[str]) -> Optional[Environment]:
+        """ Search up the Environment hierarchy for the first key in the path,
+        then travel down the path from there """
+        return self._descend(path)
 
-    def __getitem__(self, key) -> ChildEnvironment:
-        if key in self.local:
-            return self.local[key]
-        raise KeyError(f'"{key}" not defined')
+    def __getitem__(self, key: str) -> ChildEnvironment:
+        env = self._resolve_path(normalize_path(key))
+        if env is None:
+            raise KeyError(f'"{key}" not defined')
+        return env
     
     def __contains__(self, key) -> bool:
-        return key in self.local
+        return self._resolve_path(normalize_path(key)) is not None
 
     def __repr__(self):
         return self.global_name + '{' + ','.join(self.local.keys()) + '}'
@@ -55,20 +78,13 @@ class ChildEnvironment(Environment):
     def path(self) -> tuple[str]:
         return (*self.parent.path, self.name)
     
-    def resolve(self, key) -> Environment:
-        if key in self.local:
-            return self
-        return self.parent.resolve(key)
+    def _resolve(self, key: str):
+        """ Travel up the Environment hierarchy,
+        returning the environment that contains this key """
+        return super()._resolve(key) or self.parent._resolve(key)
 
-    def get(self, key, default = None) -> ChildEnvironment:
-        if key in self.local:
-            return self.local[key]
-        return self.parent.get(key, default)
-    
-    def __getitem__(self, key) -> ChildEnvironment:
-        if key in self.local:
-            return self.local[key]
-        return self.parent[key]
-    
-    def __contains__(self, key) -> bool:
-        return key in self.local or (key in self.parent)
+    def _resolve_path(self, path: tuple[str]) -> ChildEnvironment:
+        """ Search up the Environment hierarchy for the first key in the path,
+        then travel down the path from there """
+        env = self._resolve(path[0]) if len(path) > 0 else self
+        return env._descend(path)
