8db785cba266690bb828c33496795b247564003b
Benjamin Earl <14208829+benjameep@users.noreply.github.com>
2024-02-06 15:34:06 -0700

    integrate the format classes

--- kye/engine/formats.py
+++ kye/engine/formats.py
@@ -4,72 +4,8 @@ from typing import Optional, Literal
 from pandas._typing import Dtype
 import re
 
-def as_boolean(col: pd.Series, format: str):
-    if pd.api.types.infer_dtype(col) == 'boolean':
-        return col
-    return col.where(col.apply(pd.api.types.is_bool), np.nan)
-
-def as_string(col: pd.Series, format: str):
-    if pd.api.types.is_string_dtype(col):
-        return col
-    return col.astype(str, errors='ignore')
-
-def parse_number_format(format_str: str):
-    def cast_int(n: str):
-        return int(n) if n is not None and n != '' else None
-    m = re.search(r'^(-?\d+)?(,?)(?:\.(-?\d+))?$', format_str)
-    assert m is not None, 'Invalid format string'
-    start = cast_int(m.group(1))
-    thousands = m.group(2) or None
-    end = cast_int(m.group(3))
-
-    if start is not None and end is not None:
-        assert (start + end) > 0, 'Must have at least 1 significant figure'
-    return {
-        'start': start,
-        'thousands': thousands,
-        'end': end,
-    }
-
-def as_number(col: pd.Series, format: str):
-    # TODO: compute a correctly sized dtype based on the format
-    # and cast the column using that dtype, only marking it as invalid
-    # if the value wouldn't fit in that dtype
-    format = parse_number_format(format)
-    if pd.api.types.is_numeric_dtype(col):
-        valid = col.notnull()
-        if format['start'] is not None:
-            # TODO: Use the correct base for binary, octal, and hex values
-            valid &= col < (10 ** format['start'])
-        if format['end'] is not None:
-            if format['end'] > 0 and pd.api.types.is_integer_dtype(col):
-                valid = False
-            elif format['end'] == 0:
-                valid &= col % 1 == 0
-        valid |= col == 0
-        return col.where(valid, np.nan)
-    else:
-        col = col.astype(str)
-        split = col.str.extract(r'^([+-]?)([0-9,]*)(?:\.([0-9]*))?$')\
-            .set_axis(['sign','whole','decimal'], axis=1)
-        invalid = split.isna().all(axis=1)
-        split = split.fillna('')
-        if format['thousands'] == ',':
-            invalid |= ~(split.whole.str.fullmatch('^[0-9]{1,3}(,[0-9]{3})*$') | ~split.whole.str.contains(','))
-            col = col.str.replace(',','')
-        else:
-            invalid |= split.whole.str.contains(r'\D', regex=True)
-        if format['start'] is not None:
-            invalid |= split.whole.str.lstrip('0').str.len() > max(format['start'], 0)
-            if format['start'] < 0:
-                invalid |= split.decimal.str.slice(stop=-format['start']).str.contains(r'[1-9]')
-        if format['end'] == 0:
-            invalid |= split.decimal.str.rstrip('0').str.len() > 0
-        return pd.to_numeric(col.where(~invalid, np.nan))
-
-
 class Format:
-    pandas_dtype: Dtype
+    dtype: Dtype
 
     """ Abstract class for formats """
     def __init__(self, format_str: str):
@@ -81,7 +17,30 @@ class Format:
     def coerce(self, col: pd.Series) -> pd.Series: # coerced value
         raise NotImplementedError()
 
+class BooleanFormat(Format):
+    dtype = np.bool_
 
+    def validate(self, col: pd.Series) -> pd.Series:
+        errors = pd.Series(index=col.index, dtype=str)
+        col = col.astype(str).str.lower()
+        errors.loc[~col.isin(['true','false','0.0','1.0','0','1',''])] = 'WARNING_NOT_A_BOOLEAN'
+        return errors
+
+    def coerce(self, col: pd.Series) -> pd.Series:
+        if not pd.api.types.is_numeric_dtype(col):
+            return col.astype(str).str.lower().isin(['true','1','1.0'])
+        return col.astype(bool)
+
+class StringFormat(Format):
+    dtype = np.str_
+
+    def validate(self, col: pd.Series) -> pd.Series:
+        errors = pd.Series(index=col.index, dtype=str)
+        return errors
+    
+    def coerce(self, col: pd.Series) -> pd.Series:
+        return col.astype(str)
+        
 
 class NumberFormatString:
     start: Optional[int]
@@ -127,7 +86,7 @@ class NumberFormatString:
         else:
             return np.float
 
-class NumberFormat:
+class NumberFormat(Format):
     format: NumberFormatString
 
     def __init__(self, format_str: str):
@@ -165,8 +124,12 @@ class NumberFormat:
         return errors
 
     def coerce(self, col: pd.Series) -> pd.Series: # coerced value
-        if self.format.thousands and pd.api.types.is_string_dtype(col):
-            col = col.str.replace(',','')
+        if not pd.api.types.is_numeric_dtype(col):
+            col = col.astype(str)
+            if self.format.thousands:
+                col = col.str.replace(',','')
+            if self.format.is_integer:
+                col = col.str.split('.', expand=True)[0]
         col = col.astype(self.dtype)
         if self.format.end:
             if self.format.end >= 0:
