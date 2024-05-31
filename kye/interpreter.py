4a3e8656380d426f676287fadaf784e371e8ee39
Benjamin Earl <14208829+benjameep@users.noreply.github.com>
2024-05-31 14:50:47 -0600

    use ibis instead of pandas

--- kye/interpreter.py
+++ kye/interpreter.py
@@ -1,52 +1,124 @@
 from __future__ import annotations
 import typing as t
 import kye.expressions as ast
-import kye.types as types
-import pandas as pd
+import ibis
+from copy import copy
+
 from kye.errors import ErrorReporter, KyeRuntimeError
 
 if t.TYPE_CHECKING:
     from kye.engine import Engine
 
+    Indexes = t.List[t.List[str]]
+
+
+def flatten_indexes(indexes: Indexes) -> t.List[str]:
+    return list({index for index_list in indexes for index in index_list})
+
+class Type:
+    """ Abstract class for types """
+    name: str
+
+    def call(self, arguments):
+        raise NotImplementedError()
+
+    def has(self, name: str):
+        raise NotImplementedError()
+
+    def get(self, name: str):
+        raise NotImplementedError()
+    
+    def filter(self, arguments):
+        raise NotImplementedError()
+    
+    def __str__(self):
+        return "<type>"
+
+class Model(Type):
+    name: str
+    indexes: Indexes
+    methods: t.Dict[str, ast.Expr]
+    table: ibis.Table
+
+    def __init__(self, interpreter: Interpreter, name: str, indexes: Indexes, methods: t.Dict[str, ast.Expr], table: ibis.Table):
+        self.interpreter = interpreter
+        self.name = name
+        self.indexes = indexes
+        self.methods = methods
+        self.table = table
+    
+    def _filter(self, table) -> Model:
+        return Model(self.interpreter, self.name, self.indexes, copy(self.methods), table)
+    
+    def has(self, name: str):
+        return name in self.methods or name in self.table.columns
+    
+    def get(self, name: str):
+        if name in self.table.columns:
+            return self.table[name]
+        if name in self.methods:
+            col = self.interpreter.visit_with_this(self.methods[name], self)
+            col = col.name(name)
+            return col
+        return None
+    
+    def call(self, arguments) -> Model:
+        assert len(self.indexes) == 1, 'Only one index supported.'
+        index = self.indexes[0]
+        assert len(arguments) == len(index), 'Expected one argument for each index.'
+        return self._filter(self.table.filter([
+            self.table[key] == val
+            for key, val in zip(index, arguments)
+        ]))
+    
+    def __str__(self):
+        return self.table.mutate(**{
+            edge: t.cast(ibis.Value, self.get(edge))
+            for edge in self.methods
+        }).__str__()
+
 class Interpreter(ast.Visitor):
-    types: t.Dict[str, types.Type]
-    this: t.Optional[types.Type]
-    tables: Engine
+    engine: Engine
+    table_index_map: t.Dict[str, Indexes]
     reporter: ErrorReporter
+    this: t.Optional[Type]
+    types: t.Dict[str, Type]
+    calculated_edges: t.Dict[str, ibis.Column]
     
-    def __init__(self, tables: Engine,):
-        self.types = {
-            'Number': types.Number(),
-            'String': types.String(),
-            'Boolean': types.Boolean()
-        }
+    def __init__(self, engine: Engine):
+        self.engine = engine
+        self.types = {}
         self.this = None
-        self.tables = tables
     
+    def visit_with_this(self, node: ast.Node, this: Type):
+        previous = self.this
+        self.this = this
+        result = self.visit(node)
+        self.this = previous
+        return result
+        
     def visit_model(self, model_ast: ast.Model):
         if len(model_ast.indexes) == 0:
             raise KyeRuntimeError(model_ast.name, 'Models without indexes are not yet supported.')
         if len(model_ast.indexes) > 1:
             raise KyeRuntimeError(model_ast.name, 'Models with multiple indexes are not yet supported.')
+        # TODO: Type check all indexes have a corresponding edge defined
 
         model_name = model_ast.name.lexeme
-        indexes = [index.lexeme for index in model_ast.indexes[0].names]
+        indexes: Indexes = []
+        for index_ast in model_ast.indexes:
+            indexes.append([index.lexeme for index in index_ast.names])
         if len(indexes) == 0:
             raise KyeRuntimeError(model_ast.name, 'Model must have at least one index.')
 
-        if not self.tables.has_table(model_name):
+        if not self.engine.has_table(model_name):
             raise KyeRuntimeError(model_ast.name, 'Table not found for model.')
 
-        data = self.tables.get_table(model_name)
-        model = types.Model(model_name, indexes, data)
+        table = self.engine.get_table(model_name)
+        model = Model(self, model_name, indexes, {}, table)
         self.types[model_name] = model
-        self.this = model
-        self.visit(model_ast.body)
-        self.this = None
 
-        for idx_ast in model_ast.indexes[0].names:
-            if idx_ast.lexeme not in model.edges:
-                raise KyeRuntimeError(idx_ast, 'Index edge not defined in model.')
+        self.visit_with_this(model_ast.body, model)
     
     def visit_edge(self, edge_ast: ast.Edge):
         
@@ -56,46 +128,24 @@ class Interpreter(ast.Visitor):
             raise KyeRuntimeError(edge_ast.name, 'Edges with parameters are not yet supported.')
         params = []
 
-        assert isinstance(self.this, types.Model), 'No type to define edge on.'
+        assert self.this is not None
+        assert isinstance(self.this, Model)
 
-        if edge_name in self.this.edges:
-            raise KyeRuntimeError(edge_ast.name, 'Edge already defined in model.')
+        if edge_name in self.this.methods:
+            raise KyeRuntimeError(edge_ast.name, 'Edge has already been defined.')
 
-        type = self.visit(edge_ast.body)
-        if not isinstance(type, types.Type):
-            const_type = None
-            if isinstance(type, (int, float)):
-                const_type = self.types['Number']
-            elif isinstance(type, str):
-                const_type = self.types['String']
-            elif isinstance(type, bool):
-                const_type = self.types['Boolean']
-            else:
-                raise ValueError(f'Unknown type {type}')
-            type = types.Const(type, const_type)
-
-        edge = types.Edge(
-            name=edge_name,
-            model=self.this,
-            params=params,
-            cardinality=ast.Cardinality(edge_ast.cardinality.lexeme),
-            type=type
-        )
-        self.this.edges[edge_name] = edge
-
-        val = edge.call([self.this])
-        edge.type.test(val)
-        edge.bind(val)
+        # TODO: only add as a method if it is a calculated field
+        self.this.methods[edge_name] = edge_ast.body
     
     def visit_type_identifier(self, type: ast.TypeIdentifier):
-        if type.name.lexeme not in self.types:
-            raise KyeRuntimeError(type.name, 'Type not defined.')
-        return self.types[type.name.lexeme]
+        return self.types.get(type.name.lexeme)
     
     def visit_edge_identifier(self, edge: ast.EdgeIdentifier):
         if self.this is None:
             raise KyeRuntimeError(edge.name, 'Edge used outside of model.')
-        return self.this.edges[edge.name.lexeme]
+        if not self.this.has(edge.name.lexeme):
+            raise KyeRuntimeError(edge.name, f'Edge {edge.name.lexeme} not defined.')
+        return self.this.get(edge.name.lexeme)
     
     def visit_literal(self, literal: ast.Literal):
         return literal.value
@@ -103,6 +153,8 @@ class Interpreter(ast.Visitor):
     def visit_binary(self, binary: ast.Binary):
         left = self.visit(binary.left)
         right = self.visit(binary.right)
+        if left is None or right is None:
+            return None
         if binary.operator.type == ast.TokenType.PLUS:
             return left + right
         if binary.operator.type == ast.TokenType.MINUS:
@@ -130,19 +182,20 @@ class Interpreter(ast.Visitor):
         raise ValueError(f'Unknown operator {binary.operator.type}')
     
     def visit_call(self, call: ast.Call):
-        callee = self.visit(call.callee)
-        assert isinstance(callee, types.Callable), 'Can only call functions and methods.'
+        type = self.visit(call.callee)
+        assert isinstance(type, Type), 'Can only call types.'
         arguments = [self.visit(argument) for argument in call.arguments]
-        if len(arguments) != callee.arity():
-            raise ValueError(f'Expected {callee.arity()} arguments, got {len(arguments)}.')
-        return callee.call(arguments)
+        if type is None and len(arguments) == 0:
+            return arguments[0]
+        return type.call(arguments)
 
     def visit_get(self, get: ast.Get):
-        object = self.visit(get.object)
+        type = self.visit(get.object)
         edge_name = get.name.lexeme
-        assert isinstance(object, types.Model), 'Can only access edges on models.'
-        assert edge_name in object.edges, f'Edge {edge_name} not found on {object.name}.'
-        return object.edges[edge_name]
+        assert isinstance(type, Type), 'Can only access edges on tables.'
+        if not type.has(get.name.lexeme):
+            raise KyeRuntimeError(get.name, f'Edge {get.name.lexeme} not defined.')
+        return type.get(edge_name)
     
     def visit_filter(self, filter: ast.Filter):
         raise NotImplementedError('Filter expressions are not yet supported.')
