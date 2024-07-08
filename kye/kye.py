ce3b2d5a61e5d339d833dd24979fe1a09f61f2de
benjameep <14208829+benjameep@users.noreply.github.com>
2024-07-08 14:52:02 -0600

    load kye class with config obj

--- kye/kye.py
+++ kye/kye.py
@@ -1,4 +1,6 @@
 import typing as t
+from dataclasses import dataclass
+from pathlib import Path
 
 import kye.parse.expressions as ast
 import kye.type.types as typ
@@ -9,12 +11,20 @@ from kye.errors import ErrorReporter, KyeRuntimeError
 from kye.compiler import compile, write_compiled
 from kye.vm.vm import VM
 
+@dataclass
+class Config:
+    kye_file: str
+    data_file: t.Optional[str] = None
+    model_name: t.Optional[str] = None
+    compiled_out: t.Optional[str] = None
+
 class Kye:
     reporter: ErrorReporter
     type_builder: TypeBuilder
     vm: VM
 
-    def __init__(self):
+    def __init__(self, config: Config):
+        self.config = config
         self.type_builder = TypeBuilder()
     
     def parse_definitions(self, source: str) -> t.Optional[ast.Script]:
@@ -26,14 +36,14 @@ class Kye:
             return None
         return tree
 
-    def parse_expression(self, source: str) -> t.Optional[ast.Expr]:
-        """ Parse an expression from source code """
-        self.reporter = ErrorReporter(source)
-        parser = Parser(self.reporter)
-        tree = parser.parse_expression(source)
-        if self.reporter.had_error:
-            return None
-        return tree
+    # def parse_expression(self, source: str) -> t.Optional[ast.Expr]:
+    #     """ Parse an expression from source code """
+    #     self.reporter = ErrorReporter(source)
+    #     parser = Parser(self.reporter)
+    #     tree = parser.parse_expression(source)
+    #     if self.reporter.had_error:
+    #         return None
+    #     return tree
 
     def build_types(self, tree: t.Optional[ast.Node]) -> t.Optional[typ.Types]:
         """ Build types from the AST """
@@ -51,12 +61,15 @@ class Kye:
         if types is None:
             return False
         compiled = compile(types)
-        write_compiled(compiled, '.compiled.kye.yaml')
-        loader = Loader(compiled, self.reporter)
-        loader.read_jsonl('User', 'data/User.jsonl')
-        self.vm = VM(loader)
-        self.vm.reporter = self.reporter
-        self.vm.validate('User')
+        if self.config.compiled_out is not None:
+            write_compiled(compiled, self.config.compiled_out)
+        if self.config.model_name is not None:
+            loader = Loader(compiled, self.reporter)
+            assert self.config.data_file is not None
+            loader.read(self.config.model_name, self.config.data_file)
+            self.vm = VM(loader)
+            self.vm.reporter = self.reporter
+            self.vm.validate(self.config.model_name)
         return not self.reporter.had_error
     
     # def eval_expression(self, source: str) -> t.Any:
