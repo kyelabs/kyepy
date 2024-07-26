abb4303f64290424d347b9bfaa857094209c60e9
benjameep <14208829+benjameep@users.noreply.github.com>
2024-07-26 10:53:10 -0600

    add only_run_debug option

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ import pandas as pd
 DIR = Path(__file__).resolve().parent
 TEST_DIR =  DIR / 'tests'
 
+ONLY_RUN_DEBUG = False
+
 if __name__ == "__main__":
     for file in TEST_DIR.glob('**/*.yaml'):
         print(str(file.relative_to(DIR)))
@@ -16,6 +18,8 @@ if __name__ == "__main__":
             print(' ', test_case['feature'])
             for test in test_case['tests']:
                 print('   ', test['test'])
+                if ONLY_RUN_DEBUG and not test.get('debug'):
+                    continue
                 kye = Kye()
                 success = kye.compile(test_case['schema'])
                 if not success:
@@ -24,11 +28,10 @@ if __name__ == "__main__":
                 for model_name, rows in test['data'].items():
                     kye.load_df(model_name, pd.DataFrame(rows))
                 errors = kye.reporter.errors
-                if test.get('debug'):
-                    breakpoint()
                 if len(errors) > 0 and test['valid']:
-                    for error in errors:
-                        print('     ', error)
+                    kye.reporter.report()
                     raise Exception('Invalid when should have been valid')
                 if len(errors) == 0 and not test['valid']:
                     raise Exception('Valid when should have been invalid')
+                if ONLY_RUN_DEBUG:
+                    kye.reporter.report()
