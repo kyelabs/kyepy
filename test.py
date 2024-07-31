57477e9fa0a03d5a4d137c25b974c6b8661ad25e
benjameep <14208829+benjameep@users.noreply.github.com>
2024-07-31 11:11:22 -0600

    update tester to print only relevant test locations

--- test.py
+++ test.py
@@ -17,6 +17,27 @@ TEST_DIR =  DIR / 'tests'
 
 ONLY_RUN_DEBUG = args.debug
 
+class Printer:    
+    def __init__(self):
+        self.loc = (None, None)
+    
+    def update_loc(self, file, feature):
+        if self.loc[0] != file:
+            print(file.relative_to(DIR))
+            self.loc = (file, None)
+        if self.loc[1] != feature:
+            print(' ', feature)
+            self.loc = (file, feature)
+    
+    def success(self, file, feature, test):
+        self.update_loc(file, feature)
+        print('   ✅ ' + test)
+    
+    def failure(self, file, feature, test):
+        self.update_loc(file, feature)
+        print('   ❌ ' + test)
+        
+
 def lookup(df, query: dict):
     mask = pd.Series(True, index=df.index)
     for prop, values in query.items():
@@ -27,41 +48,59 @@ def lookup(df, query: dict):
     return mask
 
 if __name__ == "__main__":
+    printer = Printer()
     for file in TEST_DIR.glob('**/*.yaml'):
-        print(str(file.relative_to(DIR)))
         test_cases = yaml.safe_load(file.open('r'))
         if test_cases is None:
             continue
         for test_case in test_cases:
-            print(' ', test_case['feature'])
             for test in test_case['tests']:
                 if ONLY_RUN_DEBUG and not test.get('debug'):
                     continue
-                print('   ', test['test'])
                 kye = Kye()
-                success = kye.compile(test_case['schema'])
-                if not success:
+                successful_compilation = kye.compile(test_case['schema'])
+                
+                # Check for successful compilation
+                if not successful_compilation:
+                    printer.failure(file, test_case['feature'], test['test'])
                     kye.reporter.report()
                     raise Exception('Failed to compile schema')
+                
+                # Load the data
                 for model_name, rows in test['data'].items():
                     kye.load_df(model_name, pd.DataFrame(rows))
+                
                 error_df = t.cast(ValidationErrorReporter, kye.reporter).error_df.copy()
                 error_df['unused'] = True
                 expected_errors = test.get('errors', [])
+                
+                # Check if we expected any errors
                 if len(expected_errors) == 0 and kye.reporter.had_error:
+                    printer.failure(file, test_case['feature'], test['test'])
                     kye.reporter.report()
                     raise Exception('Invalid when should have been valid')
+
+                # Check if each error is present
                 for err in expected_errors:
                     mask = lookup(error_df, err)
                     if not mask.any():
+                        printer.failure(file, test_case['feature'], test['test'])
                         kye.reporter.report()
                         print(err)
                         print(error_df)
                         raise Exception('Expected error not found')
                     error_df.loc[mask, 'unused'] = False
+
+                # Check if there are any unexpected errors
                 if error_df['unused'].any():
+                    printer.failure(file, test_case['feature'], test['test'])
                     kye.reporter.report()
                     print(error_df[error_df['unused']].drop(columns=['unused']))
                     raise Exception('Found unexpected errors')
+
+                # Success!
+                printer.success(file, test_case['feature'], test['test'])
+
+                # If debugging then print the errors
                 if ONLY_RUN_DEBUG:
                     kye.reporter.report()
